7bb900580ba00ae97464cc8b47e6de4aad1cbda9
EduTel <15623704+EduTel@users.noreply.github.com>
2021-06-29 18:31:02 -0500

    end

--- index.py
+++ index.py
@@ -1,25 +1,47 @@
 from pprint import pprint
 from flask import Flask, request
 from lib.translate import Translate
+from flask import json
 
 app = Flask(__name__)
-o_translate = Translate(orden="sxocqnmwpfyheljrdgui",foo_letters="udxsmpf")
+o_translate = Translate(orden="sxocqnmwpfyheljrdgui", foo_letters="udxsmpf")
+
 
 @app.route("/parse", methods=['POST'])
 def hello_world():
-    array_text =  request.form.get('text').split()
-    lst_preposition = [text for text in array_text if o_translate.preposition(text)]
-    lst_verbs = [text for text in array_text if o_translate.verbs(text)]
-    lst_inflected_in_its_subjunctive = [text for text in array_text if o_translate.inflected_in_its_subjunctive(text)]
-    lst_number_to_be_pretty = [num for num in array_text if o_translate.number_to_be_pretty(num)]
-    lst_herucodes_alphabetical_order = o_translate.herucodes_alphabetical_order(array_text)
-    return {
-        "prepositions": len(lst_preposition),
-        "verbs": len(lst_verbs),
-        "subjunctive_verbs": len(lst_inflected_in_its_subjunctive),
-        "pretty_numbers": len(lst_number_to_be_pretty),
-        "vocabulary_list": lst_herucodes_alphabetical_order
+    """
+    functionality.
+
+    @rtype: dict
+    """
+    data_return = {
+        "prepositions": "",
+        "verbs": "",
+        "subjunctive_verbs": "",
+        "pretty_numbers": "",
+        "vocabulary_list": ""
     }
+    array_text = request.json.get('text')
+    if type(array_text) is str:
+        array_text = array_text.split()
+        if len(array_text) > 0:
+            lst_preposition = [text for text in array_text if o_translate.preposition(text)]
+            lst_verbs = [text for text in array_text if o_translate.verbs(text)]
+            lst_inflected_in_its_subjunctive = [text for text in array_text if o_translate.inflected_in_its_subjunctive(text)]
+            lst_number_to_be_pretty = [num for num in array_text if o_translate.number_to_be_pretty(num)]
+            lst_herucodes_alphabetical_order = o_translate.herucodes_alphabetical_order(array_text)
+            data_return["prepositions"] = len(lst_preposition)
+            data_return["verbs"] = len(lst_verbs)
+            data_return["subjunctive_verbs"] = len(lst_inflected_in_its_subjunctive)
+            data_return["pretty_numbers"] = len(lst_number_to_be_pretty)
+            data_return["vocabulary_list"] = lst_herucodes_alphabetical_order
+    response = app.response_class(
+        response=json.dumps(data_return),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- lib/translate.py
+++ lib/translate.py
@@ -1,54 +1,101 @@
 class Translate():
+    """translate according to the rules."""
 
     def __init__(self, orden: str = "", foo_letters: str = "", bar_letters: str = ""):
-        self.inf = {
-            "orden": "sxocqnmwpfyheljrdgui",
-            "foo letters": "udxsmpf",
-            "bar letters": ""
-        }
-        if bar_letters=="":
-            self.inf["bar letters"] = [ letter for letter in self.inf["orden"] if letter not in self.inf["foo letters"]]
+        """Inicialize de instance.
 
-    def alphabet(self, word: str=""):
+        @param orden: str of all alphabet
+        @type orden: str
+
+        @param foo_letters: str of only foo letters
+        @type foo_letters: str
+
+        @param bar_letters: str of only ber letters
+        @type bar_letters: str
+        """
+        self.orden = orden
+        self.foo_letters = foo_letters
+        self.bar_letters = bar_letters
+        if bar_letters == "":
+            self.bar_letters = [letter for letter in self.orden if letter not in self.foo_letters]
+
+    def alphabet(self, word: str = ""):
+        """Detect if is a alphabet valid.
+
+        @param word: a word to verify
+        @type word: str
+
+        @rtype: bool
+        """
         for data in word:
-            if data not in self.inf["orden"]:
+            if data not in self.orden:
                 return False
-        if len(word)>1:
-            return True
-        else:
-            return False
+        return len(word) > 1
 
     def preposition(self, word: str = ""):
-        if self.alphabet(word) and  len(word) == 6 and word[-1:] in self.inf["foo letters"]:
-            u_letter = [ letter for letter in word if letter in ["u"] ]
-            if len(u_letter)==0:
-                return True
+        """Detect if is a preposition.
+
+        @param word: a word to verify
+        @type word: str
+
+        @rtype: bool
+        """
+        if self.alphabet(word) and len(word) == 6 and word[-1:] in self.foo_letters:
+            u_letter = [letter for letter in word if letter in ["u"]]
+            return len(u_letter) == 0
         return False
 
     def verbs(self, word: str = ""):
-        if self.alphabet(word) and len(word) >= 6 and word[-1:] in self.inf["bar letters"]:
-            return True
-        return False
+        """Detect if is a verbs.
 
-    def inflected_in_its_subjunctive(self, word):
-        if self.verbs(word) and word[:1] in self.inf["bar letters"]:
-            return True
-        return False
+        @param word: a word to verify
+        @type word: str
 
-    def parse_herucode_to_decimal(self, herucode):
+        @rtype: bool
+        """
+        return self.alphabet(word) and len(word) >= 6 and word[-1:] in self.bar_letters
+
+    def inflected_in_its_subjunctive(self, word: str):
+        """Detect if is a subjunctive.
+
+        @param word: a word to verify
+        @type word: str
+
+        @rtype: bool
+        """
+        return self.verbs(word) and word[:1] in self.bar_letters
+
+    def parse_herucode_to_decimal(self, herucode: str):
+        """Parse language to decimal.
+
+        @param herucode: a word to parse
+        @type herucode: str
+
+        @rtype: bool
+        """
         if self.alphabet(herucode):
             suma = 0
             contador = 0
             for num in herucode:
-                suma = suma + self.inf["orden"].find(num)*(20**contador)
-                contador = contador + 1
+                suma += self.orden.find(num) * (20**contador)
+                contador += 1
             return suma
         return False
 
-    def number_to_be_pretty(self, herucode):
-        num = self.parse_herucode_to_decimal(herucode)
-        if num and num>=81827 and num%3==0:
-            return num
+    def number_to_be_pretty(self, herucode: str):
+        """Detect if is a pretty number.
 
-    def herucodes_alphabetical_order(self, words=[]):
-        return sorted(words,  key=lambda word: [self.inf["orden"].index(c) if c in self.inf["orden"] else ord(c) for c in word])
+        @rtype: integer or None
+        """
+        num = self.parse_herucode_to_decimal(herucode)
+        return num and num >= 81827 and num % 3 == 0
+
+    def herucodes_alphabetical_order(self, words: dict = []):
+        """Order alphabetically.
+
+        @param herucode: a word to parse
+        @type herucode: str
+
+        @rtype: list
+        """
+        return sorted(words, key=lambda word: [self.orden.index(c) if c in self.orden else ord(c) for c in word])
